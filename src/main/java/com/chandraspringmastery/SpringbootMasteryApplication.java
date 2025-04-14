5e57b638d1b90cdbe0e9d209715490b35777f13e
Chandra Sekhar Vipparla <124060820+ChandraSekhar-VCS@users.noreply.github.com>
2025-04-15 01:12:00 +0530

    Initial Commit

---
+++ src/main/java/com/chandraspringmastery/SpringbootMasteryApplication.java
@@ -0,0 +1,12 @@
+package com.chandraspringmastery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMasteryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootMasteryApplication.class, args);
+    }
+}
+
